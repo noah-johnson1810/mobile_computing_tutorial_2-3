cad8dd7f0d2dfdc946573dabbeb98b56ac2e1e11
Noah Johnson <njohnson@omnitech-inc.com>
2024-01-19 09:51:37 -0700

    Project setup

---
+++ app/src/main/java/edu/sdsmt/puzzle_johnson_na/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.sdsmt.puzzle_johnson_na;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
